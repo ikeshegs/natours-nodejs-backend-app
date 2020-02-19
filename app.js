6eb2ebb176b87c16a4bf977ea29de9e4d14a59fb
Okoro Ikechukwu <ikeshegs@gmail.com>
2020-02-19 01:44:57 +0100

    implement cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,7 +30,12 @@ app.set('views', path.join(__dirname, 'views'));
 /*
  *GLOBAL MIDDLEWARES
  */
+// Implement CORS
+app.use(cors());
 
+app.options('*', cors());
+
+// Compress file size
 app.use(compression());
 
 // serving static files
